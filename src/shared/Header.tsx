be73d49699c725a2aec24eb75e0509f66f5e0e30
houssemeddine.zaiem-ext <houssemeddine.zaiem-ext@mousquetaires.com>
2021-07-27 02:56:42 +0200

    refactor: refactor Header component.

--- src/shared/Header.tsx
+++ src/shared/Header.tsx
@@ -5,17 +5,11 @@ import logo from '../assets/book.png';
 const Header = () => {
   return (
     <header>
-      <nav className="navbar" role="navigation" aria-label="main navigation">
+      <nav className="navbar is-spaced" role="navigation" aria-label="main navigation">
         <div className="navbar-brand">
-          <a className="navbar-item" href="">
-            <img src={logo}/>
-          </a>
-
-          <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-          </a>
+          <figure className="image is-48x48">
+            <img src={logo} alt='logo' />
+          </figure>
         </div>
 
         <div id="navbarBasicExample" className="navbar-menu">

45bd276c898b6d087767034abbaf3204842dd601
houssemeddine.zaiem-ext <houssemeddine.zaiem-ext@mousquetaires.com>
2021-07-27 04:04:21 +0200

    feat: provide app with orders.

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,28 @@ import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Products from './views/Products';
 import Cart from './views/Cart';
 import Home from './views/Home';
+import {OrderProvider} from './order/OrderContext';
 
 const App = () => {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <div className="container">
-        <Header></Header>
-          <Switch>
-              <Route path="/products">
-                <Products />
-              </Route>
-              <Route path="/cart">
-                <Cart />
-              </Route>
-              <Route path="/">
-                <Home />
-              </Route>
-          </Switch>
-      </div>
-    </Router>
+    <OrderProvider>
+      <Router basename={process.env.PUBLIC_URL}>
+        <div className="container">
+          <Header></Header>
+            <Switch>
+                <Route path="/products">
+                  <Products />
+                </Route>
+                <Route path="/cart">
+                  <Cart />
+                </Route>
+                <Route path="/">
+                  <Home />
+                </Route>
+            </Switch>
+        </div>
+      </Router>
+    </OrderProvider>
   );
 }
 
